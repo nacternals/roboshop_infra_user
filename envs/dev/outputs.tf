023f49e47a39c17ba9960b139f06b1e13f092e9f
devops_01 <devops_01@gmail.com>
2026-03-03 14:11:23 +0530

    rabbitmq userdata.sh.tpl content has been modified....

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -33,118 +33,118 @@ output "nat_gateway_ids" {
   value = module.network.nat_gateway_ids
 }
 
-# ############################
-# # Security outputs
-# ############################
-# output "bastion_sg_id" {
-#   value = module.security.bastion_sg_id
-# }
+############################
+# Security outputs
+############################
+output "bastion_sg_id" {
+  value = module.security.bastion_sg_id
+}
 
-# output "alb_public_sg_id" {
-#   value = module.security.alb_public_sg_id
-# }
+output "alb_public_sg_id" {
+  value = module.security.alb_public_sg_id
+}
 
-# output "nginx_sg_id" {
-#   value = module.security.nginx_sg_id
-# }
+output "nginx_sg_id" {
+  value = module.security.nginx_sg_id
+}
 
-# output "alb_internal_sg_id" {
-#   value = module.security.alb_internal_sg_id
-# }
+output "alb_internal_sg_id" {
+  value = module.security.alb_internal_sg_id
+}
 
-# output "app_sg_id" {
-#   value = module.security.app_sg_id
-# }
+output "app_sg_id" {
+  value = module.security.app_sg_id
+}
 
-# output "mongodb_sg_id" {
-#   value = module.security.mongodb_sg_id
-# }
+output "mongodb_sg_id" {
+  value = module.security.mongodb_sg_id
+}
 
-# output "mysql_sg_id" {
-#   value = module.security.mysql_sg_id
-# }
+output "mysql_sg_id" {
+  value = module.security.mysql_sg_id
+}
 
-# output "redis_sg_id" {
-#   value = module.security.redis_sg_id
-# }
+output "redis_sg_id" {
+  value = module.security.redis_sg_id
+}
 
-# output "rabbitmq_sg_id" {
-#   value = module.security.rabbitmq_sg_id
-# }
+output "rabbitmq_sg_id" {
+  value = module.security.rabbitmq_sg_id
+}
 
-# ############################
-# # IAM outputs
-# ############################
-# output "iam_role_name" {
-#   value = module.iam.role_name
-# }
+############################
+# IAM outputs
+############################
+output "iam_role_name" {
+  value = module.iam.role_name
+}
 
-# output "iam_role_arn" {
-#   value = module.iam.role_arn
-# }
+output "iam_role_arn" {
+  value = module.iam.role_arn
+}
 
-# output "iam_instance_profile_name" {
-#   value = module.iam.instance_profile_name
-# }
+output "iam_instance_profile_name" {
+  value = module.iam.instance_profile_name
+}
 
-# output "iam_instance_profile_arn" {
-#   value = module.iam.instance_profile_arn
-# }
+output "iam_instance_profile_arn" {
+  value = module.iam.instance_profile_arn
+}
 
-# output "iam_policy_arn" {
-#   value = module.iam.policy_arn
-# }
+output "iam_policy_arn" {
+  value = module.iam.policy_arn
+}
 
-# ############################
-# # Bastion outputs
-# ############################
-# output "ansadmin_pubkey_ssm_parameter_name" {
-#   value = local.ansadmin_pubkey_ssm_parameter_name
-# }
+############################
+# Bastion outputs
+############################
+output "ansadmin_pubkey_ssm_parameter_name" {
+  value = local.ansadmin_pubkey_ssm_parameter_name
+}
 
-# output "bastion_instance_id" {
-#   value = module.bastion.instance_id
-# }
+output "bastion_instance_id" {
+  value = module.bastion.instance_id
+}
 
-# output "bastion_public_ip" {
-#   value = module.bastion.public_ip
-# }
+output "bastion_public_ip" {
+  value = module.bastion.public_ip
+}
 
-# output "bastion_public_dns" {
-#   value = module.bastion.public_dns
-# }
+output "bastion_public_dns" {
+  value = module.bastion.public_dns
+}
 
-# output "bastion_private_ip" {
-#   value = module.bastion.private_ip
-# }
+output "bastion_private_ip" {
+  value = module.bastion.private_ip
+}
 
-# ############################
-# # DB outputs
-# ############################
-# output "mongodb_private_ip" {
-#   value = module.mongodb.private_ip
-# }
+############################
+# DB outputs
+############################
+output "mongodb_private_ip" {
+  value = module.mongodb.private_ip
+}
 
-# output "mysql_private_ip" {
-#   value = module.mysql.private_ip
-# }
+output "mysql_private_ip" {
+  value = module.mysql.private_ip
+}
 
-# output "redis_private_ip" {
-#   value = module.redis.private_ip
-# }
+output "redis_private_ip" {
+  value = module.redis.private_ip
+}
 
-# output "rabbitmq_private_ip" {
-#   value = module.rabbitmq.private_ip
-# }
+output "rabbitmq_private_ip" {
+  value = module.rabbitmq.private_ip
+}
 
-# output "db_private_ips" {
-#   value = {
-#     mongodb  = module.mongodb.private_ip
-#     mysql    = module.mysql.private_ip
-#     redis    = module.redis.private_ip
-#     rabbitmq = module.rabbitmq.private_ip
-#   }
-# }
+output "db_private_ips" {
+  value = {
+    mongodb  = module.mongodb.private_ip
+    mysql    = module.mysql.private_ip
+    redis    = module.redis.private_ip
+    rabbitmq = module.rabbitmq.private_ip
+  }
+}
 
 # ############################
 # # DNS / ALB outputs
